22e5cadbf1d3805caa1755625e4f27399d5325e2
Julia Caro <juliac.3195@gmail.com>
2022-03-20 12:59:00 -0300

    Primer commit de adivino.py

---
+++ adivino.py
@@ -0,0 +1,16 @@
+## Adivina adivinador....
+import random
+numero_aleatorio = random.randrange(100)
+gane = False
+print("Tenés 3 intentos para adivinar un número entre 0 y 99")
+intento = 1
+while intento < 4 and not gane:
+    numeroIngresado = int(input('Ingresa tu número: '))
+    if numeroIngresado == numero_aleatorio:
+        print('Ganaste! y necesitaste {} intentos!!!'.format(intento))
+        gane = True
+    else:
+        print('Mmmm ... No.. ese número no es... Seguí intentando.')
+        intento += 1
+if not gane:
+    print('\n Perdiste :(\n El número era: {}'.format(numero_aleatorio))
