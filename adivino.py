2c38e3897950defe3222b96607b97876faa7aeb0
Julia Caro <juliac.3195@gmail.com>
2022-03-20 13:04:51 -0300

    Ahora hay 5 intentos para adivinar

--- adivino.py
+++ adivino.py
@@ -2,9 +2,9 @@
 import random
 numero_aleatorio = random.randrange(101)
 gane = False
-print("Tenés 3 intentos para adivinar un número entre 0 y 100")
+print("Tenés 5 intentos para adivinar un número entre 0 y 100")
 intento = 1
-while intento < 4 and not gane:
+while intento < 6 and not gane:
     numero_ingresado = int(input('Ingresa tu número: '))
     if numero_ingresado == numero_aleatorio:
         print('Ganaste! y necesitaste {} intentos!!!'.format(intento))
